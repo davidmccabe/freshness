4a03e5e224c053326d5939bfe108b27a70106a7b
David McCabe <davemccabe@gmail.com>
2012-04-07 16:14:49 -0700

    Added Core Data according to the instructions at http://www.theappcodeblog.com/2011/08/08/add-core-data-to-an-existing-project-in-xcode-4/

---
+++ Lastid/Food.h
@@ -0,0 +1,18 @@
+//
+//  Food.h
+//  Lastid
+//
+//  Created by David McCabe on 4/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Food : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSDate * lastAdded;
+
+@end
---
+++ Lastid/Food.m
@@ -0,0 +1,17 @@
+//
+//  Food.m
+//  Lastid
+//
+//  Created by David McCabe on 4/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Food.h"
+
+
+@implementation Food
+
+@dynamic name;
+@dynamic lastAdded;
+
+@end
