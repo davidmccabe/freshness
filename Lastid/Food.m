d917ac079265c5800f716ea9c6815e5a362a8483
David McCabe <davemccabe@gmail.com>
2012-04-07 18:47:23 -0700

    Some initial model code, but will be replaced with MagicalRecord.

--- Lastid/Food.m
+++ Lastid/Food.m
@@ -7,11 +7,34 @@
 //
 
 #import "Food.h"
-
+#import "DMAppDelegate.h"
 
 @implementation Food
 
 @dynamic name;
 @dynamic lastAdded;
+
++ (void)enterFoodWithName:(NSString *)name inContext:(NSManagedObjectContext *)context
+{
+    
+}
+
++ (Food *)foodWithName:(NSString *)name inContext:(NSManagedObjectContext *)context
+{
+    NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Food" inManagedObjectContext:context];
+    NSFetchRequest *request = [NSFetchRequest new];
+    [request setEntity:entityDescription];
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name == '%@'", name];
+    [request setPredicate:predicate];
+        
+    NSError *error = nil;
+    NSArray *results = [context executeFetchRequest:request error:&error];
+    if (results == nil || [results count] == 0) {
+        return nil;
+    } else {
+        return [results objectAtIndex:0];
+    }
+}
 
 @end
