fa67efde6f9cb553f64b8bad9227d2a7383c357d
David McCabe <davemccabe@gmail.com>
2012-04-13 23:22:29 -0700

    Animation improvements.

--- Lastid/DMDismissReviewSegue.m
+++ Lastid/DMDismissReviewSegue.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
 #import "DMDismissReviewSegue.h"
 
 @implementation DMDismissReviewSegue
@@ -15,11 +16,30 @@
     UIView *destView = [self.destinationViewController view];
     UIView *sourceView = [[self.sourceViewController navigationController] view];
     
+    sourceView.layer.borderColor = [UIColor grayColor].CGColor;
+    sourceView.layer.borderWidth = 1.0f;
+    
     [sourceView.superview insertSubview:destView belowSubview:sourceView];
+
     [UIView animateWithDuration:0.5
         animations:^{
+            sourceView.transform = CGAffineTransformMakeScale(0.2,0.2);
+        }
+        completion:^(BOOL finished) {
+            [self performStepTwo];
+        }
+     ];
+}
+
+- (void)performStepTwo
+{
+    UIView *destView = [self.destinationViewController view];
+    UIView *sourceView = [[self.sourceViewController navigationController] view];
+    
+    [UIView animateWithDuration:0.5
+        animations:^{
+            sourceView.transform = CGAffineTransformMakeScale(0.1,0.1);
             sourceView.center = CGPointMake(285,55);
-            sourceView.transform = CGAffineTransformMakeScale(0.05,0.05);
             sourceView.alpha = 0.5;
         }
         completion:^(BOOL finished) {
@@ -27,11 +47,6 @@
             [self.sourceViewController dismissViewControllerAnimated:NO completion:nil];
         }
      ];
-}
-
-- (void)performStepTwo
-{
-
 }
 
 @end
