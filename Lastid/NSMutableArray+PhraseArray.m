4678d1ee4992de73f2c3a2be4e54cd2c289b6cd8
David McCabe <davemccabe@gmail.com>
2012-07-01 16:45:07 -0700

    Fixed a bug where the first iteration of phraseArrayByJoiningExistingPhrases could put an empty string in the result.

--- Lastid/NSMutableArray+PhraseArray.m
+++ Lastid/NSMutableArray+PhraseArray.m
@@ -51,7 +51,7 @@
 		if ([DMFood foodExistsWhoseNameBeginsWith:[attemptedNextPhrase phraseString]]) {
             phrase = attemptedNextPhrase;
 		} else {
-            [result addObject:phrase];
+            if(phrase.count) [result addObject:phrase];
 			phrase = [NSMutableArray arrayWithObject:word];
         }
 	}
