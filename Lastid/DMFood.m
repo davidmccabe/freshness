3d78435b37ea3467291fa16afe3c1d788391e5fb
David McCabe <davemccabe@gmail.com>
2012-07-05 17:55:20 -0700

    Doc comment for enterFoodWithName:.

--- Lastid/DMFood.m
+++ Lastid/DMFood.m
@@ -8,6 +8,12 @@
 @dynamic name;
 @dynamic lastAdded;
 
+/*
+ For a valid food name, ensures that a food entity with that
+ name exists and that its lastAdded date is the current date.
+ Returns the food or NULL if the given name is invalid.
+ (As of writing only the empty string is invalid.)
+*/
 + (DMFood *)enterFoodWithName:(NSString *)name
 {
     DMFood *food = [DMFood MR_findFirstByAttribute:@"name" withValue:name];
