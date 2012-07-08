309f68ddab2d91abd983903901bd0b6808512660
David McCabe <davemccabe@gmail.com>
2012-07-07 19:55:00 -0700

    Improved stile of unit test.

--- Tests/DMFoodListTest.m
+++ Tests/DMFoodListTest.m
@@ -28,26 +28,23 @@
 
 - (void)testAddFoodsFromString
 {
-    NSDictionary *cases = [NSDictionary dictionaryWithObjectsAndKeys:
-        // Test existing names, non-existing names, and names that begin with
-        // the same word as an existing name but don't continue in kind.
-        @"Green Beans Ambergris Fresh Sheep Milk Fresh Lettuce Juice",
-        [NSArray arrayWithObjects:@"Green Beans", @"Ambergris", @"Fresh Sheep Milk", @"Fresh", @"Lettuce", @"Juice", nil],
-
-        // Empty string should give an empty list.
-        @"",
-        [NSArray array],
-        
-        // Redundent whitespace and commas should be removed; words should be capitalized.
-        @"lower-case letters, commas    etc.",
-        [NSArray arrayWithObjects:@"Lower-Case", @"Letters", @"Commas", @"Etc.", nil],
-    nil];
-    
-    [cases enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+    void (^testCase)(NSString *, NSArray *) = ^(NSString *string, NSArray *expected) {
         DMFoodList *list = [[DMFoodList alloc] init];
-        [list addFoodsFromString:obj];
-        GHAssertEqualObjects(list.foodNames, key, nil);
-    }];
+        [list addFoodsFromString:string];
+        GHAssertEqualObjects(list.foodNames, expected, nil);
+    };
+
+    // Test existing names, non-existing names, and names that begin with
+    // the same word as an existing name but don't continue in kind.
+    testCase(@"Green Beans Ambergris Fresh Sheep Milk Fresh Lettuce Juice",
+        [NSArray arrayWithObjects:@"Green Beans", @"Ambergris", @"Fresh Sheep Milk", @"Fresh", @"Lettuce", @"Juice", nil]);
+    
+    // Empty string should give an empty list.
+    testCase(@"", [NSArray array]);
+    
+    // Redundent whitespace and commas should be removed; words should be capitalized.
+    testCase(@"lower-case letters, commas    etc.",
+        [NSArray arrayWithObjects:@"Lower-Case", @"Letters", @"Commas", @"Etc.", nil]);
 }
 
 - (void)testCommit
